766fdae5bd5d3abe1574be484fb0008754125d04
keigo <x.10.a.s.0820keigo@gmail.com>
2026-08-18 08:29:17 +0900

    add: クリック時に色を変える処理を追加

--- js/createTimetable.js
+++ js/createTimetable.js
@@ -1,7 +1,52 @@
 
+
+// クリック以外から呼び出せるように、外部に作成
+// TODO: classの付け外しに変える。そしてcssにこれ用のクラスを作る
+function crosshairHighlight(e) {
+  const td = e.target;
+  const table = e.target.closest("table");
+  $$("td", table).map(e=>e.style.backgroundColor="")
+
+  const targetTR = [...td.closest("tr").children];
+  const tdIndex = td.cellIndex;
+
+  const targetTDs = $$(`td:nth-child(${tdIndex + 1})`, td.closest("table"));
+
+  ([...targetTR, ...targetTDs])
+    .map(e=>e.style.backgroundColor="#bb4e4e")
+
+  td.style.backgroundColor="red"
+}
+
+// 空の時間割を作る関数
+function createTimetable2() {
+  const week = "月火水木金";
+  const time = n => `${1}限目`
+
+  const timetable = range(5).map(_=>range(5))
+
+  // // 時間追加
+  // timetable.map((e,i)=>e.unshift(i==0 ? "" : i));
+
+  const trs = timetable
+    .map((e, n)=>e.map(e=>create("td", null, {classList: `time-${n+1}`, events: { click: crosshairHighlight }})))
+    .map(Wrap("tr"));
+
+  // 曜日追加
+  trs.unshift(create("tr", week.split("").map(Wrap("td"))));
+
+  // ここに移動！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+  // // 時間追加
+  // timetable.map((e,i)=>e.unshift(i==0 ? "" : i));
+
+  const timetable_table = create("table", trs, {id: "timetable"});
+
+  return timetable_table;
+}
+
 function createTimetable(timetable) {
   const week = "月火水木金";
-  const transpose = (arr) => arr[0].map((col, i) => arr.map((row) => row[i]));
+  // const transpose = (arr) => arr[0].map((col, i) => arr.map((row) => row[i]));
 
   timetable = transpose(timetable);
 
@@ -11,12 +56,16 @@ function createTimetable(timetable) {
   // 時間追加
   timetable.map((e,i)=>e.unshift(i==0 ? "" : i));
 
-  const trs = timetable.map(e=>e.map(Wrap("td"))).map(Wrap("tr"));
+  const trs = timetable.map(e=>e.map(Wrap("td", {events: { click: crosshairHighlight }}))).map(Wrap("tr"));
   const timetable_table = create("table", trs, {id: "timetable"});
 
   return timetable_table;
 }
 
-
-
-
+// 時間割データを反映させる関数
+function applyTimetable(table, events) {
+  function registerToTimetable(event) {
+    // return e => append($(, table), event)
+  }
+  events.map(registerToTimetable)
+}
