70682ef494e9f682cf14a013f861c55bbe653105
keigo <x.10.a.s.0820keigo@gmail.com>
2026-08-18 01:22:19 +0900

    コメント追加、コード整理

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // 1年後期
 const timetable_1_second = [
   /* 月 */["", "", '消費者行動論', '社会学', ""],
@@ -7,7 +6,6 @@ const timetable_1_second = [
   /* 木 */["", 'プレゼミ2（競プロ）', "", "", 'Webデザイン'],
   /* 金 */['プログラミング初歩Ⅱ', "", '情報倫理', "", ""]
 ]
-
 // 2年前期
 const timetable_2_first = [
   /* 月 */["統計学入門", "", "", "プログラミング実践1", "プログラミング実践1"],
@@ -16,8 +14,7 @@ const timetable_2_first = [
   /* 木 */["", "", "基礎ゼミ", "", ""],
   /* 金 */["", "経済学", "ネットワークアプリケーション構築", "", "法学"],
 ];
-
-
+// 上部トグルボタン作成
 function timetableToggle(e) {
   const toggle = toggleGen(timetable_1_second, timetable_2_first);
 
@@ -27,11 +24,11 @@ function timetableToggle(e) {
     $("#container").append(createTimetable(timetable));
   }
 }
-
 const toggle = create("input", null, {type: "checkbox", event: {type: "change", func: timetableToggle()}})
 const toggleLabel = create("label", [toggle, "時間割切り替え"])
-
 const header = $("header")
 header.append(toggleLabel)
 
+// 時間割作成および追加
 append(`#container`, createTimetable(timetable_2_first));
+
