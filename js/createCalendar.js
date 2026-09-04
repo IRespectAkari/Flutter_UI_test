7f54aa19a6db8898a526f6c6d1bfe29857ff1e06
keigo <x.10.a.s.0820keigo@gmail.com>
2026-09-04 18:04:12 +0900

    update: カレンダーUIをもっと見やすく改良

--- js/createCalendar.js
+++ js/createCalendar.js
@@ -1,22 +1,32 @@
 
 
 
-function createCalendar(month) {
-  const days = range(MAXDAYS[month - 1], 1)
-    .map(Wrap("div", {id: `month-${month}` ,classList: ["day"]}))
+function createMonth(month) {
+  // const MAXDAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  // const MAXDAYS = range(12, 1).map(month=>new Date(2026, month, 0).getDate())
+  const MAXDAYS = new Date(2026, month, 0).getDate()
 
-// console.log(MAXDAYS.map(n=>range(n,1)))
+  // const days = range(MAXDAYS[month - 1], 1)
+  const days = range(MAXDAYS, 1)
+    // .map(Wrap("div", {id: `month-${month}` ,classList: ["day"]}))
+    // .map(Wrap("div", {classList: ["day", `month-${month}`]}))
+    // .map(n => create("div", [n, create("div", null, {classList: "shedules"})], {classList: [`day-${n}`, `month-${month}`]}))
+    // .map(Wrap("span").map(n => create("div", [n, create("div", null, {classList: "shedules"})], {classList: ["day", `month-${month}`]}))
+    .map(n => [create("span", n), create("div", null, {classList: "shedules"})])
+    .map(Wrap("div", {classList: ["day", `month-${month}`]}))
 
   const paddingDate = range(new Date(`2026/${month}/1`).getDay())
-  .map(_=>create("span", null, {classList: "paddingDate"}))
+    .map(_=>create("div", null, {classList: "paddingDate"}));
+
+  const classLists = ["month"]
+  if(paddingDate.length == 0) {
+    classLists.push("paddingDate-0");
+  }
 
   days.unshift(...paddingDate)
-console.log(days)
-  return days;
+// console.log(days)
 
-
-
-  return create("table", days, {id: "calendar"})
+  return create("div", days, {id: `month-${month}`, classList: classLists, dataset: {month: `${month}月`}})
 }
 
 
@@ -26,8 +36,7 @@ console.log(days)
 const calendarTest = create("div", null, {id: "calendarTest"})
 append("body", calendarTest)
 
-const MAXDAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-const monthList = range(12,1).map(createCalendar).map(Wrap("div", {classList: `month`}))
+const monthList = range(12,1).map(createMonth)
 
 // append("#calendarTest", days)
 append("#calendarTest", monthList)
